8f88f0dd27c30cf218e3182cdfcaf7148c0ba491
Ha Lee <halee9@gmail.com>
2017-11-11 15:38:00 -0800

    second

--- src/InputPhoneNumber.js
+++ src/InputPhoneNumber.js
@@ -1,79 +1,92 @@
 import React, { Component } from 'react';
-import { parse, format, asYouType, isValidNumber } from 'libphonenumber-js'
+import './InputPhoneNumber.css';
+
+import { asYouType } from 'libphonenumber-js'
 import { ReactInput, templateFormatter, templateParser, parseDigit } from 'input-format'
 
 const DEFAULT_TEMPLATE = 'xxx-xxx-xxxx';
+const DEFAULT_COUNTRY_CODE = 'US';
+
+const countryCodes = [
+  { code: 'US', name: 'United States' },
+  { code: 'KR', name: 'South Korea' },
+  { code: 'JP', name: 'Japan' }
+];
+
+const flagsPath = 'https://lipis.github.io/flag-icon-css/flags/4x3/';
 
 export default class InputPhoneNumber extends Component {
   state = {
     value: '',
-    ccode: 'US',
+    ccode: DEFAULT_COUNTRY_CODE,
     formatter: {},
     template: DEFAULT_TEMPLATE
   }
 
   handleChangePhoneNumber = (value) => {
-    // console.log(value)
-    //this.setState({ value });
-    // const  parsedPhone = parse(e.target.value);
-    // console.log(parsedPhone);
-    // const formattedPhone = format(parsedPhone, 'International');
-    // this.setState({ phone: formattedPhone });
-    // const phone = value;
     const formatter = new asYouType(this.state.ccode);
     formatter.input(value);
-    // console.log(formatter);
-    //const p = formatter.partially_populated_template.replace(/x/g, " ");
-    //const p = formatter.partially_populated_template && formatter.partially_populated_template.replace(/x/g, "");
     this.setState({ value, formatter, template: formatter.template || DEFAULT_TEMPLATE });
-    //this.setState({ phone: new asYouType().input(phone) });
   }
 
   handleChangeCode = (e) => {
-    this.setState({ ccode: e.target.value });
+    this.setState({ ccode: e.target.value },() => {
+        this.handleChangePhoneNumber(this.state.value);
+    });
+    this.phoneInput.focus();
   }
 
   render(){
-    const valid = isValidNumber(this.state.phone);
     return (
       <div>
-        <select
-          onChange={this.handleChangeCode}
-        >
-          <option value='US'>US</option>
-          <option value='KR'>KR</option>
-          <option value='JP'>JP</option>
-        </select>
-        <ReactInput
-          value={ this.state.value }
-          onChange={this.handleChangePhoneNumber}
-          format={ templateFormatter(this.state.template) }
-          parse={ templateParser(this.state.template, parseDigit) }
-        />
-        
-        <div>
-          Value: {this.state.value} 
+        <div className='container'>
+          <div className='select-container'>
+            <select
+              className='select'
+              value={this.state.ccode}
+              onChange={this.handleChangeCode}
+            >
+              { countryCodes.map(country => (
+                <option value={country.code}>{country.name}</option>
+              ))}
+            </select>
+            <div className='flag-container'>
+              <img
+                className='flag-image'
+                src={`${flagsPath}${this.state.ccode.toLowerCase()}.svg`}
+              />
+              <div className='select-arrow'></div>
+            </div>
+          </div>
+          <ReactInput
+            value={ this.state.value }
+            onChange={this.handleChangePhoneNumber}
+            format={ templateFormatter(this.state.template) }
+            parse={ templateParser(this.state.template, parseDigit) }
+            ref={input => this.phoneInput = input}
+          />
         </div>
-        <div>
-          Default country: {this.state.ccode} 
-        </div>
-        <div>
-          Actual country: {this.state.formatter.country} 
-        </div>
-        <div>
-          National: {this.state.formatter.national_number} 
-        </div>
-        <div>
-          National-format: {this.state.formatter.partially_populated_template} 
-        </div>
-        <div>
-          template: {this.state.formatter.template} 
-        </div>
-      <div>
-          Valid: {valid}
+        <div style={{ padding: '10px' }}>
+          <div>
+            Value: {this.state.value} 
+          </div>
+          <div>
+            Default country: {this.state.ccode} 
+          </div>
+          <div>
+            Actual country: {this.state.formatter.country} 
+          </div>
+          <div>
+            National: {this.state.formatter.national_number} 
+          </div>
+          <div>
+            National-format: {this.state.formatter.partially_populated_template} 
+          </div>
+          <div>
+            template: {this.state.formatter.template} 
+          </div>
         </div>
       </div>
-
     )
   }
 }
